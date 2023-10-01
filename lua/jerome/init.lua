b3ce2c3dcbd17d58f988e6ee07cf752f7bca635c
jeromebandril <jerome.bandril@gmail.com>
2023-10-01 23:39:24 +0200

    set tab size

--- lua/jerome/init.lua
+++ lua/jerome/init.lua
@@ -4,3 +4,5 @@ require("jerome/plugins")
 -- Enable clipboard sharing between Neovim and the system clipboard
 vim.o.clipboard = "unnamedplus"
 
+vim.cmd('set tabstop=4')
+vim.cmd('set shiftwidth=4')
